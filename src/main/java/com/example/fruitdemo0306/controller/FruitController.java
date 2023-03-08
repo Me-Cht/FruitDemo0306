fb171475867e23e6bc7994c654a26a54c7dff58c
SGPCHotan <2225660185@qq.com>
2023-03-08 17:27:58 +0800

    修正update

--- src/main/java/com/example/fruitdemo0306/controller/FruitController.java
+++ src/main/java/com/example/fruitdemo0306/controller/FruitController.java
@@ -3,16 +3,14 @@ package com.example.fruitdemo0306.controller;
 import com.example.fruitdemo0306.bean.Fruit;
 import com.example.fruitdemo0306.utils.FruitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
@@ -26,6 +24,11 @@ public class FruitController {
     public List<Fruit> getAllFruits(){
         return fruitRepository.findAll();
     }
+    @GetMapping("findById/{id}")
+    public Optional<Fruit> getDetail(@PathVariable Long id){
+        return fruitRepository.findById(id);
+    }
+
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> deleteFruit(@PathVariable Long id){
         Fruit fruit = fruitRepository.findById(id).orElse(null);
@@ -41,9 +44,9 @@ public class FruitController {
     //声明changeFruitPrice方法，使用@PathVariable获取路径参数{id}，使用@RequestBody获取请求体中的JSON数据。
     public ResponseEntity<String> changeFruitPrice(@PathVariable("id") Long id, @RequestBody Map<String, Object> body) {
         //获取JSON数据中的newPrice，并将其转换为BigDecimal类型。
-        BigDecimal newPrice = new BigDecimal(body.get("newPrice").toString());
+        BigDecimal price = new BigDecimal(body.get("price").toString());
         //调用FruitRepository的updatePriceById方法，更新水果的价格。
-        int result = fruitRepository.updatePriceById(id, newPrice);
+        int result = fruitRepository.updatePriceById(id, price);
         //如果更新失败，则返回状态码为404，响应消息为"Fruit not found."。
         if (result == 0) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Fruit not found.");
